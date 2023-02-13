03e4359f99cc7d7f24644566fb676fee56ddf5f7
Dimkpa-Daniel <danieldimmkpa1@gmail.com>
2023-02-13 01:34:10 +0100

    Made changes to the server url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -36,7 +36,7 @@ useEffect(() => {
     setLoading(true);
   
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://dall-e-s3i8.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
